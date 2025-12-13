1d7b329d29b0fa9cf1eda580a77b2de8c7c92b6e
SalahFathy25 <s93757583@gmail.com>
2025-12-13 11:02:00 +0200

    change initial theme from system to light

--- lib/logic/settings/settings_cubit.dart
+++ lib/logic/settings/settings_cubit.dart
@@ -3,8 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quiz_app/logic/settings/settings_state.dart';
 
 class SettingsCubit extends Cubit<SettingsState> {
-  SettingsCubit()
-    : super(const SettingsInitial(ThemeMode.system, Locale('en')));
+  SettingsCubit() : super(const SettingsInitial(ThemeMode.light, Locale('en')));
 
   void changeTheme(ThemeMode themeMode) {
     emit(SettingsInitial(themeMode, state.locale));
